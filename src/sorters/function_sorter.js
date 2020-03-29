cc7d5b6f2dca56d49d11ce5e148183e2b9c30fea
alex pereira <alexapereira14@gmail.com>
2020-03-29 14:15:13 -0400

    add the function_sorter and also a couple tests to make sure it works

---
+++ src/sorters/function_sorter.js
@@ -0,0 +1,19 @@
+import BaseSorter from './base_sorter';
+
+export default class FunctionSorter extends BaseSorter {
+  /**
+   * @param data @type {Array<Object>} @example [{ p1: 1, p2: 'z' }, { p1: 99, p2: 'a' }]
+   * @param functions @type {...Function} @example (a, b) => return 1, (a, b) => return -1
+   *    functions MUST be implemented to accept two items as arguments and return a number..
+   */
+  byFunctions(data, ...functions)  {
+    return this._sort(data, ...functions);
+  }
+
+  // byFunctionAsync(data, ...functions) {
+  //   return new Promise((resolve, reject) => {
+  //     let result = this._sort(data, ...functions);
+  //     resolve(result);
+  //   });
+  // }
+}
