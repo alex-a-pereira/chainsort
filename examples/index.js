cdb206208cfb98c094c44f7d5d2949464851c70a
alex pereira <alexapereira14@gmail.com>
2021-02-13 14:21:20 -0500

    doc: add better examples

---
+++ examples/index.js
@@ -0,0 +1,55 @@
+const chainsort = require('../dist');
+
+const people = [
+  {
+    firstName: 'Alex',
+    lastName: 'Pereira',
+    car: { year: 2008 }
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Pereira',
+    car: { year: 2004 }
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Johnson',
+    car: { year: 2020 }
+  },
+  {
+    firstName: 'Noelle',
+    lastName: 'Dignan',
+    car: { year: 2013 }
+  },
+  {
+    firstName: 'Billy',
+    lastName: 'Joel',
+    car: { year: 2021 }
+  }
+];
+
+const sortByLastName = (a, b) => {
+  if(a.lastName === b.lastName) return 0;
+  return a.lastName > b.lastName ? 1 : -1;
+};
+
+// sorted by firstname, then last name, then their car's year
+chainsort.sort(people, ['firstName', sortByLastName, 'car.year']);
+// [
+//   { firstName: 'Alex', lastName: 'Johnson', car: { year: 2020 } },
+//   { firstName: 'Alex', lastName: 'Pereira', car: { year: 2004 } },
+//   { firstName: 'Alex', lastName: 'Pereira', car: { year: 2008 } },
+//   { firstName: 'Billy', lastName: 'Joel', car: { year: 2021 } },
+//   { firstName: 'Noelle', lastName: 'Dignan', car: { year: 2013 } }
+// ]
+
+// sort by last name, then by first name, then by car's year
+chainsort.sort(people, ['lastName', 'firstName', 'car.year']);
+// [
+//   { firstName: 'Noelle', lastName: 'Dignan', car: { year: 2013 } },
+//   { firstName: 'Billy', lastName: 'Joel', car: { year: 2021 } },
+//   { firstName: 'Alex', lastName: 'Johnson', car: { year: 2020 } },
+//   { firstName: 'Alex', lastName: 'Pereira', car: { year: 2004 } },
+//   { firstName: 'Alex', lastName: 'Pereira', car: { year: 2008 } }
+// ]
+
