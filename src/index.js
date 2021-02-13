19b2cb96171a7b43f6e97b5ce0ee2e7f7e42c8a0
alex pereira <alexapereira14@gmail.com>
2021-02-13 11:44:42 -0500

    feat: implement property sorter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import 'regenerator-runtime';
 import '@babel/register';
 
 import FunctionSorter from './sorters/function_sorter';
+import PropertySorter from './sorters/property_sorter';
 
 const fs = new FunctionSorter();
+const ps = new PropertySorter();
 
 module.exports = {
   byFunctions: fs.byFunctions.bind(fs),
+  byProperties: ps.byProperties.bind(ps),
   promises: {
-    byFunctionsAsync: fs.byFunctionsAsync.bind(fs)
+    byFunctionsAsync: fs.byFunctionsAsync.bind(fs),
+    byPropertiesAsync: ps.byPropertiesAsync.bind(ps)
   }
 };
